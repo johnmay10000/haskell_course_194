7a2da25868452917e37be34c06b6fa1fded47579
John May <johnmay10000@gmail.com>
2014-08-17 21:27:25 +1000

    localMaxima function

--- ch3/Golf.hs
+++ ch3/Golf.hs
@@ -19,3 +19,14 @@ genList n l = takeNth (length l - n + 1) l : genList (n-1) l
 skips :: [a] -> [[a]]
 skips [] = []
 skips l = genList (length l) l
+
+
+isLocalMaxima :: Integer -> Integer -> Integer -> Bool
+isLocalMaxima a b c = (a < b)  &&  (b > c)
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima [] = []
+localMaxima (x:y:[]) = []
+-- localMaxima (x:y:z:[]) = if isLocalMaxima x y z then [y] else []
+localMaxima (x:y:z:[]) = [y | isLocalMaxima x y z]
+localMaxima (x:y:z:xs) = if isLocalMaxima x y z then y:localMaxima (y:z:xs) else localMaxima (y:z:xs)
